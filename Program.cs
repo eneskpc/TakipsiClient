ee2528b324da7b6ffafe0f8197f7193908a9e1a0
Enes KAPUCU <mr.enes.kapucu@gmail.com>
2020-08-19 14:33:30 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakipsiClient
+{
+    class Service : ServiceBase
+    {
+        public Service()
+        {
+            ServiceName = Program.ServiceName;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Program.Start(args);
+        }
+
+        protected override void OnStop()
+        {
+            Program.Stop();
+        }
+    }
+
+    class Program
+    {
+        public const string ServiceName = "TakipsiClient";
+
+        static void Main(string[] args)
+        {
+            if (!Environment.UserInteractive)
+            {
+                using (var service = new Service())
+                {
+                    ServiceBase.Run(service);
+                }
+            }
+            else
+            {
+                ManagedInstallerClass.InstallHelper(new string[] {
+                    Assembly.GetExecutingAssembly().Location
+                });
+            }
+        }
+
+        public static void Start(string[] args)
+        {
+            // onstart code here
+        }
+
+        public static void Stop()
+        {
+            // onstop code here
+        }
+    }
+
+    [RunInstaller(true)]
+    public class MyServiceInstaller : Installer
+    {
+        public MyServiceInstaller()
+        {
+            var spi = new ServiceProcessInstaller();
+            var si = new ServiceInstaller();
+
+            spi.Account = ServiceAccount.LocalSystem;
+            spi.Username = null;
+            spi.Password = null;
+
+            si.DisplayName = Program.ServiceName;
+            si.ServiceName = Program.ServiceName;
+            si.Description = "Taksi Durağı Takip Yazılımı için CallerID cihazı okuyup gönderen istemci servisi.";
+            si.StartType = ServiceStartMode.Automatic;
+
+            Installers.Add(spi);
+            Installers.Add(si);
+        }
+    }
+}
